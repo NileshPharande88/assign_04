65d10fa842a61900b0a05100cd3d2d45ad01905b
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-03-04 18:19:23 +0530

    With code factored before fixing callbacks

--- httpServer.js
+++ httpServer.js
@@ -4,6 +4,8 @@ try{
     var qs = require('querystring');
     var jsonReader = require("json-reader");
     var jsonSort = require("json-sort");
+    var jsonToText = require("json-to-text");
+    var jsonToXML = require("json-to-xml");
 
     //Checks the  required modules are available or not.
     if ((http === undefined)) throw new Error( " Can't access http module" );
@@ -11,6 +13,8 @@ try{
     if ((qs === undefined)) throw new Error( " Can't access qs module" );
     if ((jsonReader === undefined)) throw new Error( " Can't access json-reader module" );
     if ((jsonSort === undefined)) throw new Error( " Can't access json-sort module" );
+    if ((jsonToText === undefined)) throw new Error( " Can't access json-to-text module" );
+    if ((jsonToXML === undefined)) throw new Error( " Can't access json-to-xml module" );
 
     var searchMatch = function ( data, queryParam ){
         data = data.toLowerCase();
@@ -18,27 +22,63 @@ try{
         return data.search( queryParam );
     }
 
+    var createJSONFile = function (fileName, sortedArray, cb) {
 
-    var generateOutputFile = function (req, sortedArray, cb) {
+        fs.writeFile(fileName, JSON.stringify( { students: sortedArray } ), function(err) {
+            if(err) {
+                console.log("Error to create Sorted JSON");
+            } else {
+                console.log("JSON saved to sortedSource.json");
+            }
+        });
+        return cb(0);
+    }
+
+    var generateOutputFile = function (req, res, sortedArray, cb) {
         console.log("Request: ", req.headers.accept );
-
-
-
-        console.log("Return filtered data: ", filteredSortedArray);
+        if( req.headers.accept === "application/json") {
+            console.log("Accept: application/json");
+        } else if ( req.headers.accept === "application/xml") {
+            console.log("Accept: application/xml");
+        } else if ( req.headers.accept === "text/plain") {
+            console.log("Accept: text/plain");
+        }
+/*        jsonToText.TextFileCreator("destination.txt", sortedArray, function(err, res){
+             if(err){
+                console.log("Failed: ", res);
+                //callback(null, 'one failed');
+             }else{
+                console.log("Successful: ", res);
+                //callback(null, 'one success');
+             }
+        });
+ *//*       jsonToXML.XMLFileCreator("destination.xml", sortedArray, function(err, res){
+             if(err){
+                console.log("Failed: ", res);
+                //callback(null, 'two failed');
+             }else{
+                console.log("Successful: ", res);
+                //callback(null, 'two success');
+             }
+        });
+ */       createJSONFile("sortedSource.json", sortedArray, cb);
+        ;
+        ;
+        ;
+        ;
+        console.log("Return filtered data");
         res.end("Return filtered data.");
         return cb(0);
     }
 
 
-    var giveResponce = function (req, res, sortedStudentArray, cb) {
+    var sendResponse = function (req, res, sortedStudentArray, cb) {
         var qParam = qs.parse( req.url.split('?')[1] );  //Splits parameter from the URL.
 
         var filteredSortedArray = [];//new empty local elementArray to store filtered data.
-        if( qParam.q === undefined ) {//if q param is not given in url then return all the data from the json file.
-            sortedStudentArray.forEach(function (value) {
-                filteredSortedArray.push(value);
-            });
-        } else {
+        if( qParam.q === undefined ) {//if q param is not given in url then return all the data from array.
+            filteredSortedArray = sortedStudentArray;
+        } else {  //filter data according to the qParameter.
             sortedStudentArray.forEach(function (value) {//filling of the elementArray.
                 if ( searchMatch(value.fName, qParam.q) !== -1 ) {
                     filteredSortedArray.push(value);
@@ -47,8 +87,14 @@ try{
                 }
             });//end of filling of the filteredSortedArray
         }//work about qParam.q is completed and sortedArray is ready.
-
-        generateOutputFile(req, filteredSortedArray , cb);
+        console.log("sortedStudentArray: ", sortedStudentArray);
+        console.log("filteredSortedArray: ", filteredSortedArray);
+        generateOutputFile(req, res, filteredSortedArray , cb);
+        //generateOutputFile(req, res, filteredSortedArray , cb);
+        ;
+        ;
+        console.log("Return data");
+        res.end("Return data.");
     }
     
 
@@ -57,42 +103,50 @@ try{
     var server = http.createServer ( function (req,res) {
         //Checks and allows only the GET requests from the requests coming from client.
         if ( req.method === 'GET' ) {
+
             var folderName = req.url.split('/')[1]; //devided url on bas of "/".
             folderName = folderName.toLowerCase(); //Converte folder name to lower case.
-
             if ( folderName === "favicon.ico" ) {
                 res.end();   //Avoid un necessory execution of code.
             }
             else if(folderName === "students") { //Got correct folderName from URL.
                 //Read source.json file using "json-reader" module.
                 jsonReader.jsonObject("source.json", function ( err,object ) {
-                    if(err) {
-                        console.log(object); //Returns error if failed to read JSON file.
+                    if(err) {  //Throw an error if failed to read JSON file. 
                         res.end("Error in reading JSON.");
+                        throw new Error(" Error in reading JSON.");
                     }
-                    else {// Executes code further as the json file read successfully.
+                    else {  // Executes code further as the json file read successfully.
                         jsonSort.sortJSON ( object, function (err,sortedStudentArray) {
-                            if(err) {  //Returns error if failed to return sorted array of information.
-                                console.log(err);
+                            if(err) {  //throw an error if failed to return sorted array of information.
                                 res.end("Error on sorting JSON.");
-                            } else { //if array of the sorted data is returned from the module then execute following code.
-                                giveResponce(req, res, sortedStudentArray, function (err) {
-                                    ;
-                                    ;
-                                    ;
+                                throw new Error(" Error on sorting JSON.");
+                            } else {   //if array of the sorted data is returned from the module then execute following code.
+                                //server is already closed in both cases of error or successful.
+                                sendResponse(req, res, sortedStudentArray, function (err, response) {
+                                    if(err) {  //Only throw an error message by catching error object.
+                                        console.log(err);
+                                        throw new Error(" Error in sending response.");
+                                    } else {  //display success message.
+                                        console.log("Response is successfully sent");
+                                    }
+                                    //console.log("Return data in sorted json.");
+                                    //res.end("Return data in sorted json.");
                                 });
                             }
                         });//Work of sorted json is completed.
                     }
-                });//Work of reading json is completed.
-            } else {
-                console.log("Wrong URL is entered."); //Returns the message in case of wrong URL is entered. 
+                });//Callback of reading json is completed.
+            } else {  //Throw an error in case of wrong URL is entered.
                 res.end("Wrong URL is entered.");
-            }//Work of folder name is completed.
-        } else {
-            console.log("Server closed: Request is other than GET request.");
+                throw new Error(" Wrong URL is entered.");
+            }
+            //End of the processing on the GET request.
+
+        } else {  //Close server if request coming from the client is other than GET request.
             res.end("Server closed: Request is other than GET request.");
-        }//Work of GET method is completed.
+            throw new Error(" Server closed: Request is other than GET request.");
+        }
     });//Work of createServer is completed.
 
     server.listen( 1337, "127.0.0.1", function() {
--- test_client.js
+++ test_client.js
@@ -1,16 +1,29 @@
-var http = require('http');
+try {
+  var http = require('http');
 
-//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-var options = {
-  host: '127.0.0.1',
-  path: '/students/?q=Jan',
-  port: '1337',
-  method: 'GET',
-  headers: {'accept': 'application/json,application/xml,text/plain'}
-};
+  //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+  var options = {
+    host: '127.0.0.1',
+    path: '/students',
+    port: '1337',
+    method: 'GET',
+    headers: {'accept': 'text/plain'}
+  };
 
-callback = function(response) {
-  console.log("Responce: ", response);
+  var chunks;
+  var callback = function(response) {
+    console.log("Status code: ", response.statusCode);
+    console.log("Status: ", response.statusMessage);
+    response.on('data', function (chunk) {
+      //chunks += chunk;
+      console.log('BODY: '+ chunk);
+    });
+   // response.on('end', function () {
+   //   console.log('BODY: ', chunks);
+   // });
+  }
+
+  http.request(options, callback).end();
+}catch (err) {
+  console.log("Catched error: ",err);
 }
-
-http.request(options, callback).end();
